e1f0955b14f11e9ae250c4c8edce22c23861988f
Natanael <natanbezerr63@gmail.com>
2019-09-24 16:13:47 -0400

    Apenas uma solicitação

--- solicitacao.php
+++ solicitacao.php
@@ -3,7 +3,7 @@ session_start();
 if($_SESSION['msg'] == ""){
   header("Location:index.php");
 }
-include_once "ConAL.php";
+include_once "Conec_PDO.php";
 
 $soli = filter_input(INPUT_POST,'soli',FILTER_SANITIZE_STRING);
 if($soli <> ""){
@@ -12,14 +12,35 @@ if($soli <> ""){
   $obv = filter_input(INPUT_POST,'obv',FILTER_SANITIZE_STRING);
   $destino = "Arquivo acadêmico";
 
-  $sql = "INSERT INTO mensa (soli,setor,solicitacao,obv,destino,sts,a_nome,msg_d,vr) VALUES ('$soli','$setor','$solicitacao','$obv','$destino','1','','','2')";
-  $rs = mysqli_query($conn,$sql);
+$query_verificação="SELECT * FROM mensa WHERE soli LIKE '$soli' AND setor LIKE '$setor' AND solicitacao LIKE '$solicitacao' AND sts LIKE '1'";
+$stmt = $pdo->prepare("$query_verificação");
+$stmt->execute(array('id'));
+$resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  foreach($resultado as $item){
+    $sre = $item['id'];
+  }
+if(isset($sre)){
+
   header("Location:pg_ini1.php");
-  $_SESSION['ifon']="<script>alert('Mensagen enviada com sucesso')</script>";
+  $_SESSION['ifon']="<script>alert('Você já solicitou está pasta, aguarde a resposta do arquivo acadêmico.')</script>";
+
 }else{
-  header("Location:pg_ini1.php");
-  $_SESSION['ifon']="<script>alert('falha ao tentar')</script>";
+
+
+  $sql = "INSERT INTO mensa (soli,setor,solicitacao,obv,destino,sts,a_nome,msg_d,vr) VALUES ('$soli','$setor','$solicitacao','$obv','$destino','1','','','2')";
+$inse = $pdo->prepare("$sql");
+  if($inse->execute()){
+    header("Location:pg_ini1.php");
+    $_SESSION['ifon']="<script>alert('Mensagen enviada com sucesso')</script>";
+  }else{
+    header("Location:pg_ini1.php");
+    $_SESSION['ifon']="<script>alert('Falha ao tentar enviar')</script>";
+  }
 
 }
+
+}
+
+
 
  ?>
